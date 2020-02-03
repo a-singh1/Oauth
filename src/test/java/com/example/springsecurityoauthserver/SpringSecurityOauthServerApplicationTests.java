23c531b3895adab2d1336d753bd9b6cae0320d35
Animesh Singh <animesh.singh@Triconinfotech.com>
2020-02-03 17:59:55 +0530

    server

---
+++ src/test/java/com/example/springsecurityoauthserver/SpringSecurityOauthServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springsecurityoauthserver;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
